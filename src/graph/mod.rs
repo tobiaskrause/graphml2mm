ad8f7a1f6cbb301cbed11d9252e6e620b3f31360
Tobias Krause <tobias.krause@codedsoft.com>
2018-02-25 16:19:22 +0100

    added tests for GraphEvent

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -33,3 +33,38 @@ impl GraphEvent {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use graph::*;
+    use graph::GraphEvent::*;
+
+    #[test]
+    fn node_test() {
+        let node = GraphEvent::new_node( Some("A".to_string()));
+        assert_eq!(node.unwrap(), Node { id: "A".to_string()});
+    }
+
+    #[test]
+    fn node_err_test() {
+        let node = GraphEvent::new_node( None);
+        assert_eq!(node.unwrap_err(), "node: id missing".to_string());
+    }
+
+    #[test]
+    fn edge_test() {
+        let edge = GraphEvent::new_edge( Some("A".to_string()), Some("B".to_string()), Some("C".to_string()));
+        assert_eq!(edge.unwrap(), Edge { id: "A".to_string(), source: "B".to_string(), target: "C".to_string()});
+    }
+
+    #[test]
+    fn edge_err_test() {
+        let edge = GraphEvent::new_edge( None, None, None);
+        assert_eq!(edge.unwrap_err(), "edge: missing field(s) [id: None, source: None, target: None]");
+    }
+
+    #[test]
+    fn edge_err_no_id_test() {
+        let edge = GraphEvent::new_edge( None, Some("B".to_string()), Some("C".to_string()));
+        assert_eq!(edge.unwrap_err(), "edge: missing field(s) [id: None, source: Some(\"B\"), target: Some(\"C\")]");
+    }
+}
