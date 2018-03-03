e32ba8a85c9f068d517fe34d16e359ef8ceb2b44
Tobias Krause <tobias.krause@codedsoft.com>
2018-03-03 16:55:48 +0100

    first version of mm format writer

---
+++ src/graph/writer.rs
@@ -0,0 +1,72 @@
+
+use graph::*;
+use std::io::*;
+
+pub fn  mm_writer<W: Write + Seek>(writer: &mut W, events: &Vec<GraphEvent>) {
+    writeln_file_header(writer);
+    let header_position = writer.seek(SeekFrom::Current(0)).unwrap();
+    writeln_dummy_header(writer, 10);
+    let mut entries: usize = 0;
+    let mut nodes: usize = 0;
+
+    for event in events {
+        match event {
+            &GraphEvent::Edge { id: _, ref source, ref target, attrs: _ } => {
+                entries = entries + 1;
+                writeln(writer, format!("{} {} {}", source, target, "1.0" ).as_ref());
+            }
+            &GraphEvent::Node { id: _ } => {
+                nodes = nodes + 1;
+            }
+        }
+    }
+    writer.seek(SeekFrom::Start(header_position)).unwrap();
+    write_header(writer, nodes, nodes, entries);
+}
+
+fn writeln_file_header<W: Write + Seek>(writer: &mut W) {
+    writer.write(format!("{}\n", "%%MatrixMarket matrix coordinate real general").as_ref()).unwrap();
+}
+
+
+fn write_header<W: Write + Seek>(writer: &mut W, rows: usize, columns: usize, entries: usize ) {
+    writer.write(format!("{} {} {}", rows, columns, entries).as_ref()).unwrap();
+}
+
+
+fn writeln_dummy_header<W: Write + Seek>(writer: &mut W, size: usize) {
+    let dummy = vec![32; size];
+    writer.write(&dummy).unwrap();
+    write_line_end(writer);
+}
+
+fn writeln<W: Write + Seek>(writer: &mut W, line: &str) {
+    writer.write(line.as_ref()).unwrap();
+    write_line_end(writer);
+}
+
+fn write_line_end<W: Write + Seek>(writer: &mut W) {
+    writer.write(format!("\n").as_ref()).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn write_simple() {
+        let mut writer =  Cursor::new(Vec::new());
+        let events = vec!(
+            GraphEvent::Edge {id: String::from("1"), source: String::from("1"), target: String::from("2"), attrs: HashMap::new()},
+            GraphEvent::Edge {id: String::from("2"), source: String::from("2"), target: String::from("3"), attrs: HashMap::new()},
+            GraphEvent::Node {id: String::from("1")},
+            GraphEvent::Node {id: String::from("2")},
+            GraphEvent::Node {id: String::from("3")}
+        );
+        mm_writer(&mut writer, events);
+        writer.seek(SeekFrom::Start(0)).unwrap();
+        let mut s = Vec::new();
+        writer.read_to_end(&mut s).unwrap();
+        assert_eq!(String::from("%%MatrixMarket matrix coordinate real general\n3 3 2     \n1 2 1.0\n2 3 1.0\n"), String::from_utf8(s).unwrap());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,30 @@ fn main() {
     let content = String::from(r#"
     <?xml version="1.0" encoding="UTF-8"?>
     <graphml>
-        <graph id="G" edgedefault="undirected">
-            <node id="A"/>
-            <node id="B"/>
-            <node id="C"/>
-            <node id="D"/>
-            <edge id="ab" source="A" target="B"/>
-            <edge id="bc" source="B" target="C"/>
-            <edge id="cd" source="C" target="D"/>
-            <edge id="da" source="D" target="A"/>
+        <graph id="G">
+            <node id="1"/>
+            <node id="2"/>
+            <node id="3"/>
+            <node id="4"/>
+            <edge id="1-2" source="1" target="2"/>
+            <edge id="2-3" source="2" target="3"/>
+            <edge id="3-4" source="3" target="4"/>
+            <edge id="4-1" source="4" target="1"/>
         </graph>
     </graphml>"#);
 
     let buf_reader = BufReader::new(content.as_bytes());
-
-
     let events = graph::reader::graphml_reader(buf_reader).unwrap();
-
-    for event in events {
+    println!("Events:");
+    for event in &events {
         println!("{:?}", event);
     }
+    println!("Matrix file output:");
+    let mut c = Cursor::new(Vec::new());
+    graph::writer::mm_writer(&mut c, &events);
+    c.seek(SeekFrom::Start(0)).unwrap();
+    let mut s = Vec::new();
+    c.read_to_end(&mut s).unwrap();
+    println!("{}",  String::from_utf8(s).unwrap());
 
 }
